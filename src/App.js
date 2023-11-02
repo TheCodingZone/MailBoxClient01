ff04fe773f67f5914e7b1d2850b61ac51b064435
TheCodingZone <anuragtiwari14447@gamil.com>
2023-11-02 14:38:40 +0530

    First Commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 import Navbar from './Componants/Navbar/Navbar';
 import { BrowserRouter ,Route,Routes } from 'react-router-dom';
 import Signup from './Componants/Signup/Signup';
-
+import Login from './Componants/Login/Login';
+import Home from './Componants/Pages/Home';
 function App() {
   return (
    <BrowserRouter>
@@ -11,6 +12,8 @@ function App() {
    <Routes>
 
    <Route path='/Signup' element={<Signup/>}></Route>
+   <Route path='/Login' element={<Login/>}></Route>
+   <Route path='/Home' element={<Home/>}></Route>
    </Routes>
    </BrowserRouter>
   );
--- src/Componants/Navbar/Navbar.js
+++ src/Componants/Navbar/Navbar.js
@@ -12,25 +12,18 @@ const Navbar = () => {
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+    <li class="nav-item">
+        <Link  to='/Home' class="nav-link" href="#">Home</Link>
       </li>
       <li class="nav-item">
         <Link  to='/Signup' class="nav-link" href="#">Signup</Link>
       </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
       <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
+        <Link  to='/Login' class="nav-link" href="#">Login</Link>
+      </li>
+     
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Logout</a>
       </li>
     </ul>
    
